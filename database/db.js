d84d979b6134d65ff4d79c88bdfa0657c31f4c7e
Reşat Memiş <resatmemis0606@gmail.com>
2020-01-09 18:27:05 +0300

    .\database\

---
+++ database/db.js
@@ -0,0 +1,19 @@
+//database connection config
+const Sequelize = require('sequelize')
+const db = {}
+const sequelize = new Sequelize('virtual_classroom', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
